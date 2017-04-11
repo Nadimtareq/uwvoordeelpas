90a9817e2838af19f9ad2cbc44878367f2e03a9b
Sandip Bhalodia <bhalodia.sandip@gmail.com>
2017-04-11 18:33:45 +0530

    extension for firefox

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,25 +1,59 @@
 <?php
-/**
- *  TodayDevelopment
- */
 
 namespace App\Http\Controllers;
 
 use App\Models\Affiliate;
+use  App\Helpers\AffiliateHelper;
 use App\User;
 use Illuminate\Http\Request;
-
+use Sentinel;
 use App\Http\Requests;
 
 class ApiController extends Controller
 {
+
+    public function checkAuth()
+    {       
+        $return_user = $current_user = $default_user = '';
+        $current_user = Sentinel::getUser();        
+        if($current_user){
+            $return_user = $current_user;
+        }
+        else{
+            $default_user = User::where('email', 'martijn@uwvoordeelpas.nl')->first();
+            $return_user = $default_user;
+        }
+        return $return_user;
+    }
+
+    public function findProgram($userId, $url)
+    {
+        $domain = preg_replace('/^www\./', '', $url);
+        $urlPieces = explode('.', $domain);
+       
+        $affiliate = Affiliate::where('no_show', 0)
+           ->where('affiliates.name', 'LIKE', '%'.$urlPieces[0].'%')
+            ->first()
+         ;
+        
+        if (count($affiliate) == 1) {
+            $affiliateHelper = new AffiliateHelper();
+
+            $jsonArray = array(
+                'name' => $affiliate->name,
+                'url' => $affiliateHelper->getAffiliateUrl($affiliate, $userId)
+            );
+
+            return json_encode($jsonArray);
+        }
+    }
 
     public function getUsers($key)
     {
         $users = User::all();
 
         if ($key == env('API_KEY')) {
-           return $users->toJson();
+            return $users->toJson();
         } else {
             return "ACCES DENIED";
         }
