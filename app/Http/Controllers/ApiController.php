713554968337571ef813e58ef899cfe295267bb4
Sandip Bhalodia <bhalodia.sandip@gmail.com>
2017-04-12 17:32:17 +0530

    add afflink function in controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,39 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Affiliate;
-use  App\Helpers\AffiliateHelper;
+use App\Helpers\AffiliateHelper;
 use App\User;
 use Illuminate\Http\Request;
 use Sentinel;
 use App\Http\Requests;
 
-class ApiController extends Controller
-{
+class ApiController extends Controller {
 
-    public function checkAuth()
-    {       
+    public function checkAuth() {
         $return_user = $current_user = $default_user = '';
-        $current_user = Sentinel::getUser();        
-        if($current_user){
+        $current_user = Sentinel::getUser();
+        if ($current_user) {
             $return_user = $current_user;
-        }
-        else{
+        } else {
             $default_user = User::where('email', 'martijn@uwvoordeelpas.nl')->first();
             $return_user = $default_user;
         }
         return $return_user;
     }
 
-    public function findProgram($userId, $url)
-    {
+    public function findProgram($userId, $url) {
         $domain = preg_replace('/^www\./', '', $url);
         $urlPieces = explode('.', $domain);
-       
+
         $affiliate = Affiliate::where('no_show', 0)
-           ->where('affiliates.name', 'LIKE', '%'.$urlPieces[0].'%')
-            ->first()
-         ;
-        
+                ->where('affiliates.name', 'LIKE', '%' . $urlPieces[0] . '%')
+                ->first()
+        ;
+
         if (count($affiliate) == 1) {
             $affiliateHelper = new AffiliateHelper();
 
@@ -48,8 +44,7 @@ class ApiController extends Controller
         }
     }
 
-    public function getUsers($key)
-    {
+    public function getUsers($key) {
         $users = User::all();
 
         if ($key == env('API_KEY')) {
@@ -59,8 +54,7 @@ class ApiController extends Controller
         }
     }
 
-    public function getAffiliates($key)
-    {
+    public function getAffiliates($key) {
         $affiliates = Affiliate::where('no_show', 0)->get();
 
         if ($key == env('API_KEY')) {
@@ -69,4 +63,15 @@ class ApiController extends Controller
             return "ACCES DENIED";
         }
     }
+
+    public function getAffiliatesUrl() {
+        $aff_array = array();
+        $affiliates = Affiliate::where('no_show', 0)->select('name')->get();
+        foreach ($affiliates as $affiliate) {            
+            $aff_array[] = mb_strtolower($affiliate['name']);
+        }
+        $aff_array = array_filter($aff_array);
+        return json_encode($aff_array);        
+    }
+
 }
