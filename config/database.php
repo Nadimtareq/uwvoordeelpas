04929a088b55ce31cfb754f73d575dcd59ee1a23
niyaz.abbasov <abbasov.niyaz@gmail.com>
2017-05-22 12:01:06 +0400

    database.php changed

--- config/database.php
+++ config/database.php
@@ -55,14 +55,13 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'livepas_data'),
+            'database'  => env('DB_DATABASE', 'uwvoordeelpas'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'niyaz'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-            'unix_socket' => "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
         ],
 
         'pgsql' => [
