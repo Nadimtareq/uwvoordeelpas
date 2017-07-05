61bb85b55c425b78284827bc495e9f132f0993ca
Rushabh Madhu <rushabhmadhu@gmail.com>
2017-07-05 14:34:06 +0530

    change in migration alter to create

--- database/migrations/2017_07_05_083707_dinning_table_managment_table.php
+++ database/migrations/2017_07_05_083707_dinning_table_managment_table.php
@@ -11,7 +11,7 @@ class DinningTableManagmentTable extends Migration {
      * @return void
      */
     public function up() {
-        Schema::table('dinning_tables',
+        Schema::create('dinning_tables',
                 function (Blueprint $table) {
             $table->increments('id');
             $table->string('table_number');
