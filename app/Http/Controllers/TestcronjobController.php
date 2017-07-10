11e536ab6781037770eb3c89d442f44c93473f0b
Hussain Ahmad <engrhussainahmad@gmail.com>
2017-07-10 19:28:08 +0500

    All deleted files are back

---
+++ app/Http/Controllers/TestcronjobController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Helpers\DealHelper;
+
+class TestcronjobController extends Controller
+{
+    //
+    public function index()
+    {
+      DealHelper::sendNewsletterEmail();
+    }
+
+    public function test() {
+    	mail("rushabhmadhu@gmail.com","croncalled","newslater cron");
+    	DealHelper::sendNewsletterEmail();
+    	echo "test";
+    	exit;
+    }
+}
