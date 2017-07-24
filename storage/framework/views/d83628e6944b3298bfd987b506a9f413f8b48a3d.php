948053822e8c895989f6c7cba89b2a6929e01dba
Hussain Ahmad <engrhussainahmad@gmail.com>
2017-07-25 00:44:31 +0500

    TextBlock added

--- storage/framework/views/d83628e6944b3298bfd987b506a9f413f8b48a3d.php
+++ storage/framework/views/d83628e6944b3298bfd987b506a9f413f8b48a3d.php
@@ -13,6 +13,27 @@
         </div>
 
         <div class="ui divider"></div>
+        <div class="container">
+            <div class="up">
+                <div class="start">
+                    <h2>Spaart u mee voor een gratis 3 gangenmenu?</h2>
+                    <ul class="list">
+                        <li>
+                            <div class="wrap"><img src="<?php echo e(asset('images/l1.png')); ?>" alt="l" /></div>
+                            <p>1: Klik op een webshop hieronder, log in en u gaat naar de gekozen webshop.</p>
+                        </li>
+                        <li>
+                            <div class="wrap"><img src="<?php echo e(asset('images/l2.png')); ?>" alt="l" /></div>
+                            <p>2: Doe daar uw aankoop en wij krijgen automatisch een signaal als de aankoop voltooid is</p>
+                        </li>
+                        <li>
+                            <div class="wrap"><img src="<?php echo e(asset('images/l3.png')); ?>" alt="l" /></div>
+                            <p>3: Voldoet u aan de voorwaarden? Dan wordt het saldo z.s.m. op uw account gestort.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <div class="ui grid container">
             <span></span>
             <div class="center floated sixteen wide mobile ten wide computer column">
