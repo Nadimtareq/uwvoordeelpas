a57f5f13380c43450b59394186fff3f1774c76b9
Hussain Ahmad <engrhussainahmad@gmail.com>
2017-07-24 22:16:11 +0500

    Datatable files are removed

--- resources/views/reference-code.blade.php
+++ resources/views/reference-code.blade.php
@@ -66,8 +66,6 @@
 @stop
 
 @section('scripts')
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             var clipboard = new Clipboard('#clipboard');
