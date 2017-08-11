42d277c8e1ad3a8ac71b225c437bf5062128fb5e
David Inyang-Etoh <dinyangetoh@gmail.com>
2017-08-11 15:54:23 +0100

    fixed langauge toggler on search page

--- resources/views/template/header-search.blade.php
+++ resources/views/template/header-search.blade.php
@@ -1,3 +1,10 @@
+@if(isset($userAuth->lang) && !empty($userAuth->lang))
+	 {{-- */$lang=$userAuth->lang;/* --}}
+ @elseif(Session::has('language'))
+	 {{-- */$lang=Session::get('language');/* --}}
+ @else
+	 {{-- */$lang='nl';/* --}}
+ @endif
 <header id="navigation" class="root-sec white nav header">
     <div class="container">
         <div class="row">
@@ -131,6 +138,18 @@
                                             </div>
                                         </a>
                                     </li>
+                                    <li>
+											<a class="dropdown-button blog-submenu-init notranslate" id="language" href="#!" data-activates="dropdown1">
+												<img src="{{ asset('images/flags/') }}/{{ $lang }}.png" alt="flag"> {{ strtoupper($lang) }} <i class="fa fa-angle-down" aria-hidden="true"></i>
+											</a>
+											<ul id="dropdown1" class="inline-menu submenu-ul dropdown-content notranslate">
+												 <li><a href="{{ url('setlang/nl?redirect='.Request::url()) }}" data-value="nl" class="item {{ isset($userInfo->id) ? 'change-login-user-language' : '' }}"><i class="nl flag"></i> NL</a></li>
+												 <li><a href="{{ url('setlang/en?redirect='.Request::url()) }}" data-value="en" class="item {{ isset($userInfo->id) ? 'change-login-user-language' : '' }}"><i class="gb flag"></i> EN</a></li>
+												 <li><a href="{{ url('setlang/be?redirect='.Request::url()) }}" data-value="be" class="item {{ isset($userInfo->id) ? 'change-login-user-language' : '' }}"><i class="be flag"></i> BE</a></li>
+												 <li><a href="{{ url('setlang/de?redirect='.Request::url()) }}" data-value="de" class="item {{ isset($userInfo->id) ? 'change-login-user-language' : '' }}"><i class="de flag"></i> DE</a></li>
+												 <li><a href="{{ url('setlang/fr?redirect='.Request::url()) }}" data-value="fr" class="item {{ isset($userInfo->id) ? 'change-login-user-language' : '' }}"><i class="fr flag"></i> FR</a></li>
+											</ul>
+										</li>
                                 </form>
                             </ul>
                         </div>
