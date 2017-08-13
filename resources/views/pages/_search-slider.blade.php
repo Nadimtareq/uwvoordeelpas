a861310f7f781fbdb92e23ff4cd77f17dffa2c82
David Inyang-Etoh <dinyangetoh@gmail.com>
2017-08-13 15:58:47 +0100

    fixed statistics/reservations page

--- resources/views/pages/_search-slider.blade.php
+++ resources/views/pages/_search-slider.blade.php
@@ -4,7 +4,7 @@
                 <div class="col-md-12 text-center">
                     <div class="wow bounceInDown">
                         <h1 align="center">Activeer de spaarhulp en ontvang direct €5.-</h1>
-                        <p align="center">Spaar nu automatisch bij wel 2000+ webshops.
+                        <p align="center">Spaar nu automatisch bij wel 2500+ webshops.
                             Deze betalen u tot wel 10% dinertegoed bij iedere aankoop!</p>
                             @if(!$userAuth)
                             <p align="center">
